7e4064bcb62bd75210c601f550b9fe7745455986
Yana Bialkova <yana.bialkova@gmail.com>
2022-06-16 18:10:51 +0200

    Corrected commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription ="true";
+        post.subscription = true;
 
         post.birthday.date = 13;
         post.birthday.month = 6;
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -6,7 +6,7 @@ public class Post {
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
+    public boolean subscription;
     public FormDate birthday;
 }
 
